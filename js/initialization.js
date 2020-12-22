42aa7a7a82bdbbb7de5b9cb665e2abfb6e6b92ca
marilinlopez <66434047+marilinlopez@users.noreply.github.com>
2020-12-22 12:55:01 -0300

    Add files via upload

---
+++ js/initialization.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+$(document).ready(function(){
+  $('.slider').slider();
+});
